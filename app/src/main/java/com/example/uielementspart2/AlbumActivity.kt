157da072211f068a2575ee926c3818a74bb9c74a
MacasadduJ <rafaelmacasaddu99@gmail.com>
2020-11-20 16:08:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/uielementspart2/AlbumActivity.kt
@@ -0,0 +1,27 @@
+package com.example.uielementspart2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.GridView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class AlbumActivity : AppCompatActivity() {
+    var names = arrayOf("Shrek1" , "Shrek2" , "Shrek3")
+    var images = intArrayOf(R.drawable.shrek1 , R.drawable.shrek2 , R.drawable.shrek3)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album)
+
+        val gridView = findViewById<GridView>(R.id.gridView)
+        val mainAdapter = MainAdapter(this , names , images)
+        gridView.adapter = mainAdapter
+        gridView.onItemClickListener = OnItemClickListener { _ , _ , position , id ->
+            val intent = Intent(this , AlbumDetailsActivity::class.java)
+            intent.putExtra("position" , names[position])
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/uielementspart2/AlbumDetailsActivity.kt
@@ -0,0 +1,44 @@
+package com.example.uielementspart2
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
+
+class AlbumDetailsActivity : AppCompatActivity() {
+
+    @SuppressLint("ResourceType")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album_details)
+
+        val viewName = findViewById<TextView>(R.id.albumTitleTextView)
+        val viewImage = findViewById<ImageView>(R.id.albumImageView)
+
+        var albumSongs :Array<String> =arrayOf()
+        val position = intent.extras!!.getString("position")
+
+        if (position.equals("Shrek1")){
+            viewName.text = position
+            viewImage.setImageResource(R.drawable.shrek1)
+            albumSongs = arrayOf("I'm a believer", "All Star", "I'm on my way", "My beloved monster", "Hallelujah")
+        }
+        else if (position.equals("Shrek2")){
+            viewName.text = position
+            viewImage.setImageResource(R.drawable.shrek2)
+            albumSongs = arrayOf("Accidentally in Love", "Holding Out for a Hero", "Hello World", "Livin' la Vida Loca", "Funkytown", "Changes")
+        }
+        else if (position.equals("Shrek3")){
+            viewName.text = position
+            viewImage.setImageResource(R.drawable.shrek3)
+            albumSongs = arrayOf("What I've Done", "Final Showdown", "This Moment", "Do You Remember Rock 'n' Roll Radio?",
+                "Live and Let Die")
+        }
+        var adapter = ArrayAdapter(this , android.R.layout.simple_list_item_1 , albumSongs)
+        var albumDetailListView = findViewById<ListView>(R.id.albumDetailListView)
+        albumDetailListView.adapter = adapter
+    }
+}
