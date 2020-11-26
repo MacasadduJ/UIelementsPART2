2355bd4a63b7a3d6df5cb513a31888ab53570093
MacasadduJ <rafaelmacasaddu99@gmail.com>
2020-11-26 21:11:14 +0800

    Modified for Activity 6

--- app/src/main/java/com/example/uielementspart2/AlbumDetailsActivity.kt
+++ app/src/main/java/com/example/uielementspart2/AlbumDetailsActivity.kt
@@ -1,16 +1,22 @@
 package com.example.uielementspart2
 
-import android.annotation.SuppressLint
+
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.ListView
-import android.widget.TextView
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.get
+import kotlinx.android.synthetic.main.activity_album_details.*
+
 
 class AlbumDetailsActivity : AppCompatActivity() {
 
-    @SuppressLint("ResourceType")
+    private var adapter: ArrayAdapter<String>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_album_details)
@@ -40,5 +46,47 @@ class AlbumDetailsActivity : AppCompatActivity() {
         var adapter = ArrayAdapter(this , android.R.layout.simple_list_item_1 , albumSongs)
         var albumDetailListView = findViewById<ListView>(R.id.albumDetailListView)
         albumDetailListView.adapter = adapter
+        registerForContextMenu(albumDetailListView)
+}
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?)
+    {
+        super.onCreateContextMenu(menu,v,menuInfo)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.album_list_menu, menu)
     }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.delete_item -> {
+                val intent = intent
+                val position = intent.extras!!.getInt("position")
+                val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setMessage("Remove song from album?")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", DialogInterface.OnClickListener{ dialog, which ->
+                        if(position == 0){
+                            selectedSong.removeAt(info.position)
+                        }
+                        else if(position == 1){
+                            selectedSong.removeAt(info.position)
+                        }
+                        else {
+                           selectedSong.removeAt(info.position)
+                        }
+                        adapter?.notifyDataSetChanged()
+                    })
+                    .setNegativeButton("Cancel", DialogInterface.OnClickListener{
+                            dialog, which ->
+                    })
+                val alert = dialogBuilder.create()
+                alert.setTitle("Album Dialog")
+                alert.show()
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+
+
 }
--- app/src/main/java/com/example/uielementspart2/MainActivity.kt
+++ app/src/main/java/com/example/uielementspart2/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.uielementspart2
 
+import android.app.Dialog
+import android.app.ProgressDialog.show
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.AdapterView
 import android.widget.AdapterView.AdapterContextMenuInfo
+import com.google.android.material.snackbar.Snackbar
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 var selectedSong = arrayListOf<String>()
@@ -27,8 +34,11 @@ val productsArray = arrayOf(
         "This Moment",
         "Do You Remember Rock 'n' Roll Radio?",
         "Live and Let Die"
+
 )
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -51,16 +61,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId){
+        return when (item.itemId) {
             R.id.add_to_queue -> {
                 Toast.makeText(this, "Added to Queues", Toast.LENGTH_SHORT).show()
                 val info = item.menuInfo as AdapterContextMenuInfo
                 selectedSong.add(productsArray[info.position])
+                val snackbar = Snackbar.make(mainLayout,"View Listed Songs?",Snackbar.LENGTH_LONG)
+                snackbar.setAction("Go",View.OnClickListener {
+                    startActivity(Intent(applicationContext,QueueActivity::class.java))
+                })
+                snackbar.show()
                 true
             }
-            else-> super.onContextItemSelected(item)
+            else -> super.onOptionsItemSelected(item)
+            }
+
         }
-    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId){
             R.id.go_to_queue_page -> {
