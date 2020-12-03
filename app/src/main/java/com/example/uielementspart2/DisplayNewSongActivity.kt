642671c263099fea4b3e1d318e7183178117e715
MacasadduJ <rafaelmacasaddu99@gmail.com>
2020-12-03 21:12:28 +0800

    Updated display of new songs

--- app/src/main/java/com/example/uielementspart2/DisplayNewSongActivity.kt
+++ app/src/main/java/com/example/uielementspart2/DisplayNewSongActivity.kt
@@ -3,10 +3,14 @@ package com.example.uielementspart2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import com.example.uielementspart2.models.Song
 
 class DisplayNewSongActivity : AppCompatActivity() {
@@ -25,5 +29,56 @@ class DisplayNewSongActivity : AppCompatActivity() {
 
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_2, songs)
         songsListView.adapter = adapter
+        registerForContextMenu(songsListView)
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.create_song -> {
+                startActivity(Intent(this, CreateSongsActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.long_press2, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+        return when(item.itemId){
+            R.id.edit_song -> {
+                val song_id = songs[info.position].id
+                val intent = Intent(applicationContext, EditSongActivity::class.java)
+                intent.putExtra("song_id", song_id)
+                startActivity(intent)
+                true
+            }
+            R.id.delete_song -> {
+                val song = songs[info.position]
+                if(songsTableHandler.delete(song)){
+                    songs.removeAt(info.position)
+                    adapter.notifyDataSetChanged()
+                    Toast.makeText(applicationContext, "Song was deleted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT).show()
+                }
+                true
+            }
+            else -> return super.onContextItemSelected(item)
+        }
+
+
+    }
+}
+
 }
